0191f1807744659bc82c06731e7ca52e902f558b
Jalal Ahmad <jalal.ahmad@gmail.com>
2012-09-30 11:31:31 +0530

    Initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrumNow::Application.config.secret_token = '6bb992c9f4a5fe5e43a539dda7deae53c5b400105b905bf2e3dd7c660bf72048b91a72d8e25f2d78a067ff74dd2f181749e476ac293ec1adab77d842a00aa8ba'
